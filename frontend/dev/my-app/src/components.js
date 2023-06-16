8abb8f9eb9a4f6c382ba1290cff7b17a4c87a4d7
Ettore Marangon <marangonettore@gmail.com>
2023-06-16 17:36:28 +0200

    set up MUI and main component

---
+++ frontend/dev/my-app/src/components.js
@@ -0,0 +1,10 @@
+// File to build the single React components
+
+// import MUI materials
+import Container from "@mui/material/Container";
+
+const MainComponent = () => {
+  return <Container> Here comes the main component</Container>;
+};
+
+export { MainComponent };
--- frontend/playground/my-app/src/components.js
+++ frontend/playground/my-app/src/components.js
@@ -1,44 +1,66 @@
-const Greeting = () => {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
+import book_img from "./images/book_img.jpg";
+import book2_img from "./images/book2_img.jpg";
+
+const firstBook = {
+  author: "John Ironmonger",
+  title: "Der Eisbär und die Hoffnung auf morgen: Roman",
+  img: book_img,
 };
 
-const Person = () => <h3>Ettore Marangon</h3>;
-const Message = () => <h2>Hallo World</h2>;
+const secondBook = {
+  author: "Dora Heldt ",
+  title: "Liebe oder Eierlikör",
+  img: book2_img,
+};
 
 // For Book list Project
-
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 }
 
-function Book() {
+// props = properties
+function Book(props) {
+  console.log(props);
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 }
 
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/915rw9sjivL._AC_UY218_.jpg"
-    alt="Der Eisbär und die Hoffnung auf morgen: Roman"
-  />
-);
-const Title = () => <h2>Der Eisbär und die Hoffnung auf morgen: Roman</h2>;
-const Author = () => <h2> John Ironmonger </h2>;
+// const Image = () => (
+//   <img src={book_img} alt="Der Eisbär und die Hoffnung auf morgen: Roman" />
+// );
+// const Title = () => {
+//   const title = "Der Eisbär und die Hoffnung auf morgen: Roman";
+//   return <h2>{title}</h2>;
+// };
+// const Author = () => {
+//   const author = "John Ironmonger";
+//   // OBJECT Variable to pass style settings
+//   const inlineHeadingStyle = {
+//     color: "#617d98",
+//     fontSize: "0.75rem",
+//     marginTop: "0.5rem",
+//     letterSpacing: "2px",
+//   };
+//   // pass JS Object in style
+//   return <h4 style={inlineHeadingStyle}> {author}</h4>;
+// };
 
 export { BookList, Book };
