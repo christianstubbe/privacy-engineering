16f4cceeb0893dc4a6c4d77cb560972571e588c5
Christian Stubbe <christian@digitlcloud.com>
2023-06-16 16:18:25 +0200

    added terraform resouces for cloud function

--- toolbox/terraform/main.tf
+++ toolbox/terraform/main.tf
@@ -9,10 +9,40 @@ terraform {
 
 provider "google" {
     project = "tu-berlin-privacy-engineering"
-    region  = "eu-west3"
-    zone    = "eu-west1-a"
+    region  = "europe-west1"
+    zone    = "europe-west1-a"
 }
 
-resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"
+resource "google_storage_bucket" "default" {
+  name                        = "privacy-engineering-toolbox-source"
+  location                    = "EUROPE-WEST1"
+  force_destroy               = true
+}
+
+data "archive_file" "default" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "../src/"
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "toolbox-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.default.output_path # Add path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "default" {
+  name        = "privacy-engineering-toolbox"
+  location    = "europe-west1"
+
+  build_config {
+    runtime     = "python311"
+    entry_point = "hello_world"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
 }
