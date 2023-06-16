bdad983ee1ff8e5280e4736cd2bbcac487c27e97
Ettore Marangon <marangonettore@gmail.com>
2023-06-16 13:04:44 +0200

    test components in playground

--- frontend/playground/my-app/src/components.js
+++ frontend/playground/my-app/src/components.js
@@ -10,4 +10,35 @@ const Greeting = () => {
 const Person = () => <h3>Ettore Marangon</h3>;
 const Message = () => <h2>Hallo World</h2>;
 
-export default { Greeting, Person, Message };
+// For Book list Project
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+function Book() {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/915rw9sjivL._AC_UY218_.jpg"
+    alt="Der Eisbär und die Hoffnung auf morgen: Roman"
+  />
+);
+const Title = () => <h2>Der Eisbär und die Hoffnung auf morgen: Roman</h2>;
+const Author = () => <h2> John Ironmonger </h2>;
+
+export { BookList, Book };
