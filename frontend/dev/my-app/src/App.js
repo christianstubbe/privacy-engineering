741ea3c90b4776c605d8ad4186df9c7b0dcec71b
Ettore Marangon <marangonettore@gmail.com>
2023-06-17 21:19:40 +0200

    2 buttons added

--- frontend/dev/my-app/src/App.js
+++ frontend/dev/my-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { MainComponent } from "./components/Main";
 import Header from "./components/Header.js";
+import Buttons from "./components/Button.js";
 
 // Add React.Frangment?
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Buttons />
     </div>
   );
 }
