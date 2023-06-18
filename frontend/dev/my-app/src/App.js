5b004941384642fddfc8283360f38d781ba87c1c
Ettore Marangon <marangonettore@gmail.com>
2023-06-18 02:34:45 +0200

    sidebar

--- frontend/dev/my-app/src/App.js
+++ frontend/dev/my-app/src/App.js
@@ -2,15 +2,27 @@ import "./App.css";
 import { MainComponent } from "./components/Main";
 import Header from "./components/Header.js";
 import Buttons from "./components/Button.js";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Products from "./pages/Products";
 
 // Add React.Frangment?
-
+// div className="App"></div>;
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Buttons />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact components={Home} />
+          <Route path="/products" components={Products} />
+          <Route path="/reports" components={Reports} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
