8adae7f22b8960f0f06196f248482482f7837c95
Ettore Marangon <marangonettore@gmail.com>
2023-06-19 12:08:43 +0200

    navbar + user icon

--- frontend/dev/my-app/src/App.js
+++ frontend/dev/my-app/src/App.js
@@ -1,29 +1,28 @@
 import "./App.css";
 import { MainComponent } from "./components/Main";
-import Header from "./components/Header.js";
+import Navbar from "./components/Navbar.js";
 import Buttons from "./components/Button.js";
-import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
-import Home from "./pages/Home";
-import Reports from "./pages/Reports";
-import Products from "./pages/Products";
 
 // Add React.Frangment?
 // div className="App"></div>;
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact components={Home} />
-          <Route path="/products" components={Products} />
-          <Route path="/reports" components={Reports} />
-        </Routes>
-      </Router>
+      <Navbar />
     </>
   );
 }
 
 export default App;
+
+{
+  /* <Router>
+  <Navbar />
+  <Routes>
+    <Route path="/" exact components={Home} />
+    <Route path="/products" components={Products} />
+    <Route path="/reports" components={Reports} />
+  </Routes>
+</Router>; */
+}
