7f49a2827df223eb0b89861b624fc44677b96521
Ettore Marangon <marangonettore@gmail.com>
2023-06-19 19:47:03 +0200

    first draft FE + fixing bugs tailwind config

--- frontend/dev/my-app/src/components/Main.js
+++ frontend/dev/my-app/src/components/Main.js
@@ -2,9 +2,23 @@
 
 // import MUI materials
 import Container from "@mui/material/Container";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import "./Main.css";
+import Leftbox from "./Leftbox";
 
 const MainComponent = () => {
-  return <main className="main"> Here comes the main component</main>;
+  return (
+    <main className="main">
+      <div className="leftBox">
+        🛫 UPLOAD IMAGE
+        <Leftbox />
+      </div>
+      <div className="rightBox"> 🛬 RETRIEVE IMAGES</div>
+    </main>
+  );
 };
 
 export { MainComponent };
