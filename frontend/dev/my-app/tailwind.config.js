b3ad070b20a420a17033e71ebdc8792e3ebfe353
Ettore Marangon <marangonettore@gmail.com>
2023-06-19 12:40:34 +0200

    set up tailwindcss config file

---
+++ frontend/dev/my-app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // for every sub-folder and file
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
