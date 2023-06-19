69b31ea4838abf5b2dfbec8abe87b15bfa2663bc
Ettore Marangon <marangonettore@gmail.com>
2023-06-19 12:08:43 +0200

    navbar + user icon

--- frontend/dev/my-app/src/components/Navbar.js
+++ frontend/dev/my-app/src/components/Navbar.js
@@ -1,47 +1,18 @@
-import React, { useState } from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { SidebarData } from "./NavbarData";
+import React from "react";
 import "./Navbar.css";
-import { IconContext } from "react-icons";
-
-function Navbar() {
-  // with react-icons
-  const [sidebar, setSidebar] = useState(false);
-  const showSidebar = () => {
-    setSidebar(!sidebar);
-  };
-  return (
-    <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <div className="navbar">
-          <Link to="#" className="menuBars">
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
-        </div>
-        <nav className={sidebar ? "navActive" : "navMenu"}>
-          <ul className="navMenuItems" onClick={showSidebar}>
-            <li className="NavbarToggle">
-              <Link to="#" className="menuBars">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
-  );
-}
-
+import * as BiIcons from "react-icons/bi";
+// import "./Header.css";
+import logo_TU from "../images/logo_TU.png";
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="infoToolbox">
+      <img className="logo" src={logo_TU} alt="Logo TU" />
+      <h4>Internal Image Upload</h4>
+    </div>
+    {/* <img src={logo_TU} alt="TU Berlin" /> */}
+    <div className="userInfo">
+      <BiIcons.BiUserCircle className="icon" />
+    </div>
+  </nav>
+);
 export default Navbar;
