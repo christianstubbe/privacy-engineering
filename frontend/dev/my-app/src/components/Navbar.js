73e0925fe0ab365eecfcf1d9ce4af1c07025a9ab
Ettore Marangon <marangonettore@gmail.com>
2023-06-19 15:27:49 +0200

    update

--- frontend/dev/my-app/src/components/Navbar.js
+++ frontend/dev/my-app/src/components/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./Navbar.css";
 import * as BiIcons from "react-icons/bi";
+import styles from "../index.css";
 // import "./Header.css";
 import logo_TU from "../images/logo_TU.png";
 const Navbar = () => (
   <nav className="navbar">
     <div className="infoToolbox">
-      <img className="logo" src={logo_TU} alt="Logo TU" />
+      <img className="h-41 w-110" src={logo_TU} alt="Logo TU" />
       <h4>Internal Image Upload</h4>
     </div>
-    {/* <img src={logo_TU} alt="TU Berlin" /> */}
     <div className="userInfo">
       <BiIcons.BiUserCircle className="icon" />
     </div>
   </nav>
 );
 export default Navbar;
+
+// className="navbar
+// className="logo"
