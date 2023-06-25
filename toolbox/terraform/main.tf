61ef77c9033758406842613285c0c7520bf803cd
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2023-06-25 12:29:37 +0200

    * Fix duplicates in TF config

--- toolbox/terraform/main.tf
+++ toolbox/terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-    required_providers {
-        google = {
-            source  = "hashicorp/google"
-            version = ">= 4.34.0"
-        }
-    }
-}
-
 provider "google" {
     project = "tu-berlin-privacy-engineering"
     region  = "europe-west1"
