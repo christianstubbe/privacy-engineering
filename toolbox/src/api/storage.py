c10b45de46155741146b1b56a99718f6fb5bc52a
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2023-06-25 11:53:35 +0200

    * Refactor API/Connectors

--- toolbox/src/api/storage.py
+++ toolbox/src/api/storage.py
@@ -1,19 +1,22 @@
-from fastapi import APIRouter, Request, HTTPException, Depends
-import json, os, requests, logging
+from fastapi import APIRouter
+import json
+import os
+import requests
+import logging
+
 # TODO: import utilitary file for data transformation
 logger = logging.getLogger(__name__)
 
 cloud_router = APIRouter()
 
 # Bearer token
-token = os.environ.get('SERVICE_ACCOUNT_TOKEN')
+token = os.environ.get("SERVICE_ACCOUNT_TOKEN")
 if token is None:
     logger.error("Token is not set! Cannot make a request")
     # raise HTTPException(status_code=500, detail="Improperly set function!")
 
-headers = {
-    'Authorization': f'Bearer {token}'
-}
+headers = {"Authorization": f"Bearer {token}"}
+
 
 # TODO: Get RDB data with REST calls
 @cloud_router.get("/data/rdb")
@@ -21,9 +24,10 @@ async def get_relational_db():
     """
     Query data from relational database
     """
-    url = 'https://example.com/api/resource'
+    url = "https://example.com/api/resource"
     response = requests.get(url, headers=headers)
     return json.dumps("it's alive!")
+
 
 # TODO: Post RDB data with REST calls
 
