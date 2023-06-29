56f0069b76fabecca232388a244442e0cb42f10c
Christian Stubbe <christian@digitlcloud.com>
2023-06-29 12:53:21 +0200

    added theme. Refactored component structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#c50e1f'
+    }
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
