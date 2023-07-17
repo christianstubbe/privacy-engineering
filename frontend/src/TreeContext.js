ccc86aff16ebaf4030bf8566f75bdbd72721e3e7
Ettore Marangon <marangonettore@gmail.com>
2023-07-17 11:29:21 +0200

    added checkboxes into the p-tree

--- frontend/src/TreeContext.js
+++ frontend/src/TreeContext.js
@@ -30,42 +30,49 @@ const mockTreeData = [
   {
     purpose_id: 1,
     name: "All purposes",
+    selected: false,
     transformations: [],
     parent_id: null,
     children: [
       {
         purpose_id: 2,
         name: "Marketing",
+        selected: false,
         transformations: ["BLUR", "REMOVEBG"],
         parent_id: 1,
         children: [
           {
             purpose_id: 3,
             name: "Offline",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 2,
             children: [
               {
                 purpose_id: 4,
                 name: "Print Advertising",
+                selected: false,
                 transformations: ["REMOVEBG"],
                 parent_id: 3,
               },
               {
                 purpose_id: 5,
                 name: "Outdoor Advertising",
+                selected: false,
                 transformations: ["REMOVEBG"],
                 parent_id: 3,
               },
               {
                 purpose_id: 6,
                 name: "Event Marketing",
+                selected: false,
                 transformations: ["REMOVEBG"],
                 parent_id: 3,
               },
               {
                 purpose_id: 7,
                 name: "TV and Radio Advertising",
+                selected: false,
                 transformations: ["REMOVEBG"],
                 parent_id: 3,
               },
@@ -74,30 +81,35 @@ const mockTreeData = [
           {
             purpose_id: 8,
             name: "Online",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 2,
             children: [
               {
                 purpose_id: 9,
                 name: "Social Media",
+                selected: false,
                 transformations: ["BLACKWHITE"],
                 parent_id: 8,
                 children: [
                   {
                     purpose_id: 3,
                     name: "LinkedIn",
+                    selected: false,
                     transformations: ["BLACKWHITE"],
                     parent_id: 9,
                   },
                   {
                     purpose_id: 3,
                     name: "Instagram",
+                    selected: false,
                     transformations: ["BLACKWHITE"],
                     parent_id: 9,
                   },
                   {
                     purpose_id: 3,
                     name: "Facebook",
+                    selected: false,
                     transformations: ["BLACKWHITE"],
                     parent_id: 9,
                   },
@@ -106,6 +118,7 @@ const mockTreeData = [
               {
                 purpose_id: 10,
                 name: "Website",
+                selected: false,
                 transformations: ["BLACKWHITE"],
                 parent_id: 8,
               },
@@ -116,30 +129,35 @@ const mockTreeData = [
       {
         purpose_id: 11,
         name: "HR",
+        selected: false,
         transformations: [],
         parent_id: null,
         children: [
           {
             purpose_id: 12,
             name: "Recruitment",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 6,
           },
           {
             purpose_id: 13,
             name: "Payroll Processing",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 6,
           },
           {
             purpose_id: 14,
             name: "Training and Development",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 6,
           },
           {
             purpose_id: 15,
             name: "Performance Evaluation",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 6,
           },
@@ -148,30 +166,35 @@ const mockTreeData = [
       {
         purpose_id: 16,
         name: "Sales",
+        selected: false,
         transformations: ["BLACKWHITE"],
         parent_id: 2,
         children: [
           {
             purpose_id: 17,
             name: "Customer Relationship Management Access",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 11,
           },
           {
             purpose_id: 18,
             name: "Sales Order Processing",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 11,
           },
           {
             purpose_id: 19,
             name: "Sales Campaign Management",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 11,
           },
           {
             purpose_id: 20,
             name: "Sales Forecasting",
+            selected: false,
             transformations: ["BLACKWHITE"],
             parent_id: 11,
           },
@@ -180,36 +203,42 @@ const mockTreeData = [
       {
         purpose_id: 21,
         name: "Microsoft 365",
+        selected: false,
         transformations: ["REMOVEBG"],
         parent_id: 1,
         children: [
           {
             purpose_id: 22,
             name: "User Management",
+            selected: false,
             transformations: ["REMOVEBG"],
             parent_id: 21,
           },
           {
             purpose_id: 23,
             name: "Exchange Online Administration",
+            selected: false,
             transformations: ["REMOVEBG"],
             parent_id: 21,
           },
           {
             purpose_id: 24,
             name: "SharePoint Online Administration",
+            selected: false,
             transformations: ["REMOVEBG"],
             parent_id: 21,
           },
           {
             purpose_id: 25,
             name: "Microsoft Teams Administration",
+            selected: false,
             transformations: ["REMOVEBG"],
             parent_id: 21,
           },
           {
             purpose_id: 26,
             name: "License Management",
+            selected: false,
             transformations: ["REMOVEBG"],
             parent_id: 21,
           },
@@ -218,6 +247,26 @@ const mockTreeData = [
     ],
   },
 ];
+
+const handleCheckboxChange = (node) => {
+  const updatedTreeData = updateNodeSelection(mockTreeData, node);
+  // dispatch({ type: "FETCH_SUCCESS", payload: updatedTreeData });
+};
+
+const updateNodeSelection = (treeData, nodeToUpdate) => {
+  return treeData.map((node) => {
+    if (node.purpose_id === nodeToUpdate.purpose_id) {
+      return { ...node, selected: !node.selected };
+    } else if (node.children && node.children.length > 0) {
+      return {
+        ...node,
+        children: updateNodeSelection(node.children, nodeToUpdate),
+      };
+    } else {
+      return node;
+    }
+  });
+};
 
 export const TreeContext = createContext();
 
@@ -237,7 +286,11 @@ const TreeContextProvider = ({ children }) => {
 
   return (
     <TreeContext.Provider
-      value={{ treeData: mockTreeData, isLoading: state.isLoading }}
+      value={{
+        treeData: mockTreeData,
+        isLoading: state.isLoading,
+        handleCheckboxChange,
+      }}
     >
       {children}
     </TreeContext.Provider>
--- frontend/src/components/PurposeTree.js
+++ frontend/src/components/PurposeTree.js
@@ -1,14 +1,14 @@
-import React, {useContext, useEffect, useState} from 'react';
-import { TreeContext } from '../TreeContext';
-import { Chip } from '@mui/material';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import TreeView from '@mui/lab/TreeView';
-import TreeItem from '@mui/lab/TreeItem';
-
+import React, { useContext, useEffect, useState } from "react";
+import { TreeContext } from "../TreeContext";
+import { Chip } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import TreeView from "@mui/lab/TreeView";
+import TreeItem from "@mui/lab/TreeItem";
+import Checkbox from "@mui/material/Checkbox";
 
 const PurposeTree = () => {
-  const { treeData, isLoading } = useContext(TreeContext);
+  const { treeData, isLoading, handleCheckboxChange } = useContext(TreeContext);
   const [expanded, setExpanded] = useState([]);
 
   useEffect(() => {
@@ -20,31 +20,39 @@ const PurposeTree = () => {
     setExpanded(getAllNodeIds(treeData));
   }, [treeData]);
 
-  const renderLabel = (nodeId, label, transformations) => (
-    <div>
+  const renderLabel = (
+    nodeId,
+    label,
+    transformations,
+    selected,
+    handleCheckboxChange
+  ) => (
+    <div style={{ display: "flex", alignItems: "center" }}>
       <div>{label}</div>
-      <div>Transformations:
-        {transformations.map(transformation => (
-          <Chip
-            key={transformation}
-            label={transformation}
-            color={'primary'}
-          />
+      <Checkbox
+        checked={selected}
+        onChange={() => handleCheckboxChange(nodeId)}
+        color="primary"
+      />
+      <div>
+        Transformations:
+        {transformations.map((transformation) => (
+          <Chip key={transformation} label={transformation} color={"primary"} />
         ))}
-        </div>
+      </div>
     </div>
   );
 
-  const renderTree = (nodes) => (
+  const renderTree = (nodes) =>
     nodes.map((node) => (
       <TreeItem
-          key={node.purpose_id}
-          nodeId={node.purpose_id.toString()}
-          label={renderLabel(node.purpose_id, node.name, node.transformations)}>
+        key={node.purpose_id}
+        nodeId={node.purpose_id.toString()}
+        label={renderLabel(node.purpose_id, node.name, node.transformations)}
+      >
         {Array.isArray(node.children) ? renderTree(node.children) : null}
       </TreeItem>
-    ))
-  );
+    ));
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -60,6 +68,6 @@ const PurposeTree = () => {
       {renderTree(treeData)}
     </TreeView>
   );
-}
+};
 
 export default PurposeTree;
