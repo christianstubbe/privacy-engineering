7d1e6ef603863c2eccb24065a6616918a7fb64b1
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2023-07-19 09:30:58 +0200

    * Delete wrapper.py (change meant to be on another branch)

--- toolbox/src/cloud/gcp/wrapper.py
+++
@@ -1,9 +0,0 @@
-from cloud.interface import CloudAPIInterface
-import logging
-logger = logging.getLogger(__name__)
-client = bigquery.Client(credentials=credentials,
-                         project=os.getenv("GCP_PROJECT_NAME"))
-
-class GoogleWrapper(CloudAPIInterface):
-    
-    
